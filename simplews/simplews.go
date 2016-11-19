e07baae8aea99d6c46eb46b935460732e99d3477
uia-worker <janis.gailis@uia.no>
2016-11-19 19:37:15 +0100

    Added a code for simple web server.

---
+++ simplews/simplews.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, what do you want me to do with %s?", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandlerFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
